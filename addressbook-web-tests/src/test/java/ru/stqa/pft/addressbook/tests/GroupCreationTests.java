1cf9cc97fdac1208daff7810ae55fd904c7a03a7
Anton.Mitsura <mitsuraanton@mail.ru>
2016-05-13 18:30:53 +0300

    Логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -52,13 +54,12 @@ public class GroupCreationTests extends TestBase {
 
     @Test (dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
-            app.goTo().groupPage();
-            Groups before = app.group().all();
-            app.group().createGroup(group);
-            assertThat(app.group().count(), equalTo(before.size() + 1));
-            Groups after = app.group().all();
-            assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
+         app.goTo().groupPage();
+         Groups before = app.group().all();
+         app.group().createGroup(group);
+         assertThat(app.group().count(), equalTo(before.size() + 1));
+         Groups after = app.group().all();
+         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
     @Test
